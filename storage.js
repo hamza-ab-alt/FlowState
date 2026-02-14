a9c42094f1886e6f1b907eb5eefce95760c78126
hamza aballagh <hamzaaballagh941@gmail.com>
2026-02-14 22:43:11 +0100

    finished the work in the storage file and virefie my code

--- storage.js
+++ storage.js
@@ -1,3 +1,7 @@
 const KEY = "flow_tasks";
 
+export const getTasks = () =>
+  JSON.parse(localStorage.getItem(KEY)) || [];
 
+export const saveTasks = tasks =>
+  localStorage.setItem(KEY, JSON.stringify(tasks));
